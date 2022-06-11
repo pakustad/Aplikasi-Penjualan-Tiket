3ba79eba6a6c46fe3086a792be83b924c4705a7e
kangwildan <kangwildan@ymail.com>
2022-06-11 14:32:04 +0700

    Fix: delete category

--- resources/views/kategori/index.blade.php
+++ resources/views/kategori/index.blade.php
@@ -21,69 +21,81 @@
 </section>
 
 
-    <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-12">
-            <div class="card">
-              <div class="card-header">
-                <h3 class="card-title">Kategori</h3>
-              </div>
-              <!-- /.card-header -->
-              <div class="card-body">
-                    @include('notifikasi')
-                <table id="example1" class="table table-bordered table-striped">
-                  <thead>
-                  <tr>
-                    <th>No</th>
-                    <th>Nama Kategori</th>
-                    <th>Dibuat</th>
-                    <th>Aksi</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                            @php
-                            $no = 1;
-                            @endphp
-                            @foreach ($kategori as $k)
-                  <tr>
-                                <td>{{ $no }}</td>
-                                <td>{{ $k->nama_kategori }}</td>
-                                <td>{{ $k->created_at->format("d M Y") }}</td>
-                                <th class="d-flex">
-                                    <a href="{{ route('kategori.edit', $k->id) }}"
-                                        class="btn btn-sm btn-success">edit</a>
-                                    {!! Form::open(['route' =>['kategori.destroy',$k->id],'method' => 'DELETE']) !!}
-                                    <button type="submit" name="submit" class="btn btn-sm btn-danger ml-2">hapus</button>
-                                    {!! Form::close() !!}
-                                </th>
-                            </tr>
-                            @php
-                            $no++;
-                            @endphp
-                            @endforeach
-                  </tbody>
-                </table>
-              </div>
-              <!-- /.card-body -->
-            </div>
-            <!-- /.card -->
+<!-- Main content -->
+<section class="content">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-header">
+            <h3 class="card-title">Kategori</h3>
           </div>
-          <!-- /.col -->
+          <!-- /.card-header -->
+          <div class="card-body">
+            @include('notifikasi')
+            <table id="example1" class="table table-bordered table-striped">
+              <thead>
+                <tr>
+                  <th>No</th>
+                  <th>Nama Kategori</th>
+                  <th>Dibuat</th>
+                  <th>Aksi</th>
+                </tr>
+              </thead>
+              <tbody>
+                @if(count($kategori) >= 1)
+                @php
+                $no = 1;
+                @endphp
+                @foreach ($kategori as $k)
+                <tr>
+                  <td>{{ $no }}</td>
+                  <td>{{ $k->nama_kategori }}</td>
+                  <td>{{ $k->created_at->format("d M Y") }}</td>
+                  <th class="d-flex">
+                    <a href="{{ route('kategori.edit', $k->id) }}" class="btn btn-sm btn-success">edit</a>
+                    <button onclick="deleteCategory('{{ $k->id }}')" class="btn btn-sm btn-danger ml-2">hapus</button>
+                  </th>
+                </tr>
+                @php
+                $no++;
+                @endphp
+                @endforeach
+                @endif
+              </tbody>
+            </table>
+          </div>
+          <!-- /.card-body -->
         </div>
-        <!-- /.row -->
+        <!-- /.card -->
       </div>
-      <!-- /.container-fluid -->
-    </section>
-  
-    
+      <!-- /.col -->
+    </div>
+    <!-- /.row -->
+  </div>
+  <!-- /.container-fluid -->
+  <!-- Form Delete -->
+  <form action="" id="form-delete" method="post">
+    {!! Form::open(['method' => 'DELETE']) !!}
+    {!! Form::close() !!}
+  </form>
+</section>
+
+
 @endsection
 
-    @push('scripts')
-    <script>
-        $(function() {
-            $('#users-table').DataTable();
-        });
-    </script>
-    @endpush
+@section('scripts')
+<script>
+  $(function() {
+    $('#users-table').DataTable();
+  });
+</script>
+
+<script>
+  function deleteCategory(id) {
+    const formDelete = document.getElementById("form-delete");
+    formDelete.setAttribute("action", `<?= url("/kategori") ?>/${id}`);
+    document.forms["form-delete"].submit();
+  }
+</script>
+@stop
