a8bf3b58b958a3ad508ff7dce7c726c3295a7ec5
kangwildan <kangwildan@ymail.com>
2022-06-11 14:31:46 +0700

    Fix: adding scripts section

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,90 +27,95 @@
   <!-- summernote -->
   <link rel="stylesheet" href="{{ url('adminlte/plugins/summernote/summernote-bs4.min.css') }}">
 </head>
+
 <body class="hold-transition sidebar-mini layout-fixed">
-<div class="wrapper">
-@include('layouts.header')
+  <div class="wrapper">
+    @include('layouts.header')
 
 
 
-@include('layouts.sidebar')
+    @include('layouts.sidebar')
 
-<div class="content-wrapper">
-    @yield('content')
-</div>
+    <div class="content-wrapper">
+      @yield('content')
+    </div>
 
-@include('layouts.footer')
- 
-</div>
-<!-- ./wrapper -->
+    @include('layouts.footer')
 
-<!-- jQuery -->
-<script src="{{ url('adminlte/plugins/jquery/jquery.min.js') }}"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="{{ url('adminlte/plugins/jquery-ui/jquery-ui.min.js') }}"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-  $.widget.bridge('uibutton', $.ui.button)
-</script>
-<!-- Bootstrap 4 -->
-<script src="{{ url('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<!-- ChartJS -->
-<script src="{{ url('adminlte/plugins/chart.js/Chart.min.js') }}"></script>
-<!-- Sparkline -->
-<script src="{{ url('adminlte/plugins/sparklines/sparkline.js') }}"></script>
-<!-- JQVMap -->
-<script src="{{ url('adminlte/plugins/jqvmap/jquery.vmap.min.js') }}"></script>
-<script src="{{ url('adminlte/plugins/jqvmap/maps/jquery.vmap.usa.js') }}"></script>
-<!-- jQuery Knob Chart -->
-<script src="{{ url('adminlte/plugins/jquery-knob/jquery.knob.min.js') }}"></script>
-<!-- daterangepicker -->
-<script src="{{ url('adminlte/plugins/moment/moment.min.js') }}"></script>
-<script src="{{ url('adminlte/plugins/daterangepicker/daterangepicker.js') }}"></script>
-<!-- Tempusdominus Bootstrap 4 -->
-<script src="{{ url('adminlte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
-<!-- Summernote -->
-<script src="{{ url('adminlte/plugins/summernote/summernote-bs4.min.js') }}"></script>
-<!-- overlayScrollbars -->
-<script src="{{ url('adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
-<!-- AdminLTE App -->
-<script src="{{ url('adminlte/dist/js/adminlte.js')}}"></script>
-<!-- AdminLTE for demo purposes -->
-{{-- <script src="{{ url('adminlte/dist/js/demo.js')}}"></script> --}}
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="{{ url('adminlte/dist/js/pages/dashboard.js')}}"></script>
-{{-- Ionicons  --}}
-<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-<!-- DataTables  & Plugins -->
-<script src="{{ url('plugins/datatables/jquery.dataTables.min.js')}}"></script>
-<script src="{{ url('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
-<script src="{{ url('plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
-<script src="{{ url('plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
-<script src="{{ url('plugins/datatables-buttons/js/dataTables.buttons.min.js')}}"></script>
-<script src="{{ url('plugins/datatables-buttons/js/buttons.bootstrap4.min.js')}}"></script>
-<script src="{{ url('plugins/jszip/jszip.min.js')}}"></script>
-<script src="{{ url('plugins/pdfmake/pdfmake.min.js')}}"></script>
-<script src="{{ url('plugins/pdfmake/vfs_fonts.js')}}"></script>
-<script src="{{ url('plugins/datatables-buttons/js/buttons.html5.min.js')}}"></script>
-<script src="{{ url('plugins/datatables-buttons/js/buttons.print.min.js')}}"></script>
-<script src="{{ url('plugins/datatables-buttons/js/buttons.colVis.min.js')}}"></script>
-<!-- Page specific script -->
-<script>
-  $(function () {
-    $("#example1").DataTable({
-      "responsive": true, "lengthChange": false, "autoWidth": false,
-      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-    $('#example2').DataTable({
-      "paging": true,
-      "lengthChange": false,
-      "searching": false,
-      "ordering": true,
-      "info": true,
-      "autoWidth": false,
-      "responsive": true,
+  </div>
+  <!-- ./wrapper -->
+
+  <!-- jQuery -->
+  <script src="{{ url('adminlte/plugins/jquery/jquery.min.js') }}"></script>
+  <!-- jQuery UI 1.11.4 -->
+  <script src="{{ url('adminlte/plugins/jquery-ui/jquery-ui.min.js') }}"></script>
+  <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+  <script>
+    $.widget.bridge('uibutton', $.ui.button)
+  </script>
+  <!-- Bootstrap 4 -->
+  <script src="{{ url('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <!-- ChartJS -->
+  <script src="{{ url('adminlte/plugins/chart.js/Chart.min.js') }}"></script>
+  <!-- Sparkline -->
+  <script src="{{ url('adminlte/plugins/sparklines/sparkline.js') }}"></script>
+  <!-- JQVMap -->
+  <script src="{{ url('adminlte/plugins/jqvmap/jquery.vmap.min.js') }}"></script>
+  <script src="{{ url('adminlte/plugins/jqvmap/maps/jquery.vmap.usa.js') }}"></script>
+  <!-- jQuery Knob Chart -->
+  <script src="{{ url('adminlte/plugins/jquery-knob/jquery.knob.min.js') }}"></script>
+  <!-- daterangepicker -->
+  <script src="{{ url('adminlte/plugins/moment/moment.min.js') }}"></script>
+  <script src="{{ url('adminlte/plugins/daterangepicker/daterangepicker.js') }}"></script>
+  <!-- Tempusdominus Bootstrap 4 -->
+  <script src="{{ url('adminlte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
+  <!-- Summernote -->
+  <script src="{{ url('adminlte/plugins/summernote/summernote-bs4.min.js') }}"></script>
+  <!-- overlayScrollbars -->
+  <script src="{{ url('adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
+  <!-- AdminLTE App -->
+  <script src="{{ url('adminlte/dist/js/adminlte.js')}}"></script>
+  <!-- AdminLTE for demo purposes -->
+  {{-- <script src="{{ url('adminlte/dist/js/demo.js')}}"></script> --}}
+  <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+  <script src="{{ url('adminlte/dist/js/pages/dashboard.js')}}"></script>
+  {{-- Ionicons  --}}
+  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+  <!-- DataTables  & Plugins -->
+  <script src="{{ url('plugins/datatables/jquery.dataTables.min.js')}}"></script>
+  <script src="{{ url('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+  <script src="{{ url('plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
+  <script src="{{ url('plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
+  <script src="{{ url('plugins/datatables-buttons/js/dataTables.buttons.min.js')}}"></script>
+  <script src="{{ url('plugins/datatables-buttons/js/buttons.bootstrap4.min.js')}}"></script>
+  <script src="{{ url('plugins/jszip/jszip.min.js')}}"></script>
+  <script src="{{ url('plugins/pdfmake/pdfmake.min.js')}}"></script>
+  <script src="{{ url('plugins/pdfmake/vfs_fonts.js')}}"></script>
+  <script src="{{ url('plugins/datatables-buttons/js/buttons.html5.min.js')}}"></script>
+  <script src="{{ url('plugins/datatables-buttons/js/buttons.print.min.js')}}"></script>
+  <script src="{{ url('plugins/datatables-buttons/js/buttons.colVis.min.js')}}"></script>
+  <!-- Page specific script -->
+  <script>
+    $(function() {
+      $("#example1").DataTable({
+        "responsive": true,
+        "lengthChange": false,
+        "autoWidth": false,
+        "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+      }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+      $('#example2').DataTable({
+        "paging": true,
+        "lengthChange": false,
+        "searching": false,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+        "responsive": true,
+      });
     });
-  });
-</script>
+  </script>
+  @yield("scripts")
 </body>
+
 </html>
