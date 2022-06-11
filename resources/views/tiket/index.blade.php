a829f1ec7d78145b12af49a1a79bbdc687e69a37
kangwildan <kangwildan@ymail.com>
2022-06-11 14:58:24 +0700

    Fix: delete ticket

--- resources/views/tiket/index.blade.php
+++ resources/views/tiket/index.blade.php
@@ -20,76 +20,88 @@
 </section>
 
 
-    <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-12">
-            <div class="card">
-              <div class="card-header">
-                <h3 class="card-title">Tiket</h3>
-              </div>
-              <!-- /.card-header -->
-              <div class="card-body">
-                    @include('notifikasi')
-                <table id="example1" class="table table-bordered table-striped">
-                  <thead>
-                  <tr>
-                        <th>No</th>
-                        <th>Nama Tiket</th>
-                        <th>Jenis Tiket</th>
-                        <th>Kategori Tiket</th>
-                        <th>Jumlah Tiket</th>
-                        <th>Harga Tiket</th>
-                        <th>Created At</th>
-                        <th>Aksi</th>
-                  </tr>
-                  </thead>
-                        <tbody>
-                            @php
-                            $no = 1;
-                            @endphp
-                            @foreach ($tiket as $t)
-                            <tr>
-                                <td>{{ $no }}</td>
-                                <td>{{ $t->name_tiket }}</td>
-                                <td>{{ $t->jenis_tiket }}</td>
-                                <td>{{ $t->kategori->nama_kategori }}</td>
-                                <td>{{ $t->jumlah_tiket }}</td>
-                                <td> @currency($t->harga_tiket) </td>
-                                <td>{{ $t->created_at->format('d M Y') }}</td>
-                                <th class="d-flex">
-                                    <a href="{{ route('tiket.edit', $t->id) }}" class="btn btn-sm btn-success">edit</a>
-                                    {!! Form::open(['route' => ['tiket.destroy', $t->id], 'method' => 'DELETE']) !!}
-                                    <button type="submit" name="submit"
-                                        class="btn btn-sm btn-danger ml-2">hapus</button>
-                                    {!! Form::close() !!}
-                                </th>
-                            </tr>
-                            @php
-                            $no++;
-                            @endphp
-                            @endforeach
-                        </tbody>
-                </table>
-              </div>
-              <!-- /.card-body -->
-            </div>
-            <!-- /.card -->
+<!-- Main content -->
+<section class="content">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-header">
+            <h3 class="card-title">Tiket</h3>
           </div>
-          <!-- /.col -->
+          <!-- /.card-header -->
+          <div class="card-body">
+            @include('notifikasi')
+            <table id="example1" class="table table-bordered table-striped">
+              <thead>
+                <tr>
+                  <th>No</th>
+                  <th>Nama Tiket</th>
+                  <th>Jenis Tiket</th>
+                  <th>Kategori Tiket</th>
+                  <th>Jumlah Tiket</th>
+                  <th>Harga Tiket</th>
+                  <th>Created At</th>
+                  <th>Aksi</th>
+                </tr>
+              </thead>
+              <tbody>
+                @php
+                $no = 1;
+                @endphp
+                @foreach ($tiket as $t)
+                <tr>
+                  <td>{{ $no }}</td>
+                  <td>{{ $t->name_tiket }}</td>
+                  <td>{{ $t->jenis_tiket }}</td>
+                  <td>{{ $t->kategori->nama_kategori }}</td>
+                  <td>{{ $t->jumlah_tiket }}</td>
+                  <td> @currency($t->harga_tiket) </td>
+                  <td>{{ $t->created_at->format('d M Y') }}</td>
+                  <th class="d-flex">
+                    <a href="{{ route('tiket.edit', $t->id) }}" class="btn btn-sm btn-success">edit</a>
+                    <button type="button" onclick="deleteTicket('{{$t->id}}')" class="btn btn-sm btn-danger ml-2">hapus</button>
+                  </th>
+                </tr>
+                @php
+                $no++;
+                @endphp
+                @endforeach
+              </tbody>
+            </table>
+          </div>
+          <!-- /.card-body -->
         </div>
-        <!-- /.row -->
+        <!-- /.card -->
       </div>
-      <!-- /.container-fluid -->
-    </section>
-    
-    @endsection
+      <!-- /.col -->
+    </div>
+    <!-- /.row -->
+  </div>
+  <!-- /.container-fluid -->
+  <form action="" method="post" id="form-delete">
+    {!! Form::open(['method' => 'DELETE']) !!}
+    {!! Form::close() !!}
+  </form>
+</section>
 
-    @push('scripts')
-    <script>
-        $(function() {
-            $('#users-table').DataTable();
-        });
-    </script>
-    @endpush
+@endsection
+
+@push('scripts')
+<script>
+  $(function() {
+    $('#users-table').DataTable();
+  });
+</script>
+@endpush
+
+
+@section("scripts")
+<script>
+  function deleteTicket(id) {
+    const formDelete = document.getElementById("form-delete");
+    formDelete.setAttribute("action", `<?= url("/tiket") ?>/${id}`);
+    document.forms["form-delete"].submit();
+  }
+</script>
+@stop
